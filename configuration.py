119aa3cf7ec4e08111222c167bcd38c5b0582d3b
SmolinA <maf1a1496@gmail.com>
2023-08-27 08:03:59 +1000

    My first commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://3490d606-a59e-490f-b430-93c9d22c9e8a.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
